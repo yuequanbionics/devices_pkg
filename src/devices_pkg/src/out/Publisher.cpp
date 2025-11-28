c37f20c7e4cf7a42d9592f073679efb344ff9b4a
zzz10276 <zhangruiming1027@163.com>
2025-11-28 15:39:01 +0800

    增加gitignore文件

---
+++ src/devices_pkg/src/out/Publisher.cpp
@@ -0,0 +1,56 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class MinimalPublisher : public rclcpp::Node
+{
+
+public:
+    MinimalPublisher()
+
+        : Node("minimal_publisher")
+
+    {
+
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+
+        timer_ = this->create_wall_timer(
+
+            std::chrono::milliseconds(500),
+
+            std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+private:
+    void timer_callback()
+
+    {
+
+        auto message = std_msgs::msg::String();
+
+        message.data = "Hello, world! " + std::to_string(count_++);
+
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
+        publisher_->publish(message);
+    }
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    size_t count_ = 0;
+
+};
+
+int main(int argc, char *argv[])
+
+{
+
+    rclcpp::init(argc, argv);
+
+    rclcpp::spin(std::make_shared<MinimalPublisher>());
+
+    rclcpp::shutdown();
+
+    return 0;
+}
