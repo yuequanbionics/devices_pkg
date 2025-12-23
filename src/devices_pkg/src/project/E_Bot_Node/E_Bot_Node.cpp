321a9b9c0673fb35ec8dcb1d35ca8062c5c8c66a
zzz10276 <zhangruiming1027@163.com>
2025-12-23 09:55:07 +0800

    1项目模块化处理 2增加自定义消息类型的处理

---
+++ src/devices_pkg/src/project/E_Bot_Node/E_Bot_Node.cpp
@@ -0,0 +1,44 @@
+#include "E_Bot_Node.hpp"
+#include <filesystem>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+
+{
+
+    UDP_Def UDP_Def_one;
+    Init_One_UDP_Connect(&UDP_Def_one, inet_addr("127.0.0.1"), htons(16201));
+
+     Robot_Hardware *Test_Robot = new Robot_Hardware();
+    Test_Robot->Add_Device_Type("Switch_Board", Switch_Board_Device_Init, Switch_Board_Device_CallBack_F, Switch_Board_Device_Delete_F);
+    // Test_Robot->Add_Device_Type("TaiHu_Custom_Motor", Motor_Device_Init_TaiHu, Motor_Device_CallBack_TaiHu, Motor_Device_Delete_TaiHu);
+
+    // filesystem::path exe_path = filesystem::canonical("/proc/self/exe");  
+    // filesystem::path dir_path = exe_path.parent_path();
+    // std::cout << "程序所在目录: " << dir_path << std::endl;
+
+    // string ADDR = dir_path.string() + "/../config/YAML/W_Bot/zhang_RuiMing/Motor/TaiHu_TOP.yaml";
+    // if(Test_Robot->Init_TOP(ADDR) != 0)
+    // {
+    //     cout << "Init_ERR" << endl;
+    //     return -1;
+    // }
+
+    // const shared_ptr<Device_Struct> Main_Switch_Board = Test_Robot->Get_Device_For_Name("Main_Switch_Board");
+    // Main_B *Main_Switch_Board_Control = static_cast<Main_B *>(Test_Robot->Get_Control_Class(Main_Switch_Board));
+
+    // const shared_ptr<Device_Struct> Motor = Test_Robot->Get_Device_For_Name("TiaHu_Motor");
+    // Motor_TaiHu *Motor_TaiHu_c = static_cast<Motor_TaiHu *>(Test_Robot->Get_Control_Class(Motor));
+
+    rclcpp::init(argc, argv);
+
+    rclcpp::spin(std::make_shared<E_Bot_Publisher>(Test_Robot));
+    rclcpp::spin(std::make_shared<E_Bot_Subscriber>(Test_Robot));
+
+    rclcpp::shutdown();
+
+    return 0;
+}
---
+++ src/devices_pkg/src/project/W_Bot_Node/W_Bot_Node.hpp
@@ -0,0 +1,56 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "UDP.hpp"
+#include "HARDWARE_TOP.hpp"
+#include "Custom_TOP.hpp"
+#include "Switch_Board.hpp"
+#include <filesystem>
+#include <iostream>
+#include "e_bot_msg.hpp"
+
+using namespace std;
+
+class E_Bot_Publisher : public rclcpp::Node
+{
+public:
+    E_Bot_Publisher(Robot_Hardware *Robot_e_bot)
+    : Node("e_bot_publisher_node")
+    , Robot_ptr(Robot_e_bot)
+    {
+        publisher_ = this->create_publisher<devices_pkg::msg::EBotMsg>("e_bot_publisher", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(10),
+            std::bind(&E_Bot_Publisher::timer_callback, this));
+ }
+private:
+    void timer_callback()
+    {
+        auto message = devices_pkg::msg::EBotMsg();
+        message.e = 1.23;
+        message.last_data = "E_Bot Message Data";
+         publisher_->publish(message);
+    }
+    rclcpp::Publisher<devices_pkg::msg::EBotMsg>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    Robot_Hardware *Robot_ptr;
+};
+
+
+class E_Bot_Subscriber : public rclcpp::Node
+{
+public:
+     E_Bot_Subscriber(Robot_Hardware *Robot_e_bot) 
+     : Node("e_bot_subscriber_node")
+     , Robot_ptr(Robot_e_bot)
+     {
+        subscription_ = this->create_subscription<devices_pkg::msg::EBotMsg>("e_bot_subscriber", 10, std::bind(&E_Bot_Subscriber::topic_callback, this, std::placeholders::_1));
+     }
+
+private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+       // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<devices_pkg::msg::EBotMsg>::SharedPtr subscription_;
+    Robot_Hardware *Robot_ptr;
+};
