64d118e62d029cd1192481785e0eb61cadcd1909
zzz10276 <zhangruiming1027@163.com>
2025-12-30 10:19:56 +0800

    增加x-hand依赖

--- src/devices_pkg/src/project/X_Hand_Node/X_Hand_Node.cpp
+++ src/devices_pkg/src/project/X_Hand_Node/X_Hand_Node.cpp
@@ -1,90 +1,31 @@
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-//#include "Motor_TaiHu.hpp"
-#include "UDP.hpp"
-#include "HARDWARE_TOP.hpp"
-#include "Custom_TOP.hpp"
-#include "Switch_Board.hpp"
-
+#include "X_Hand_Node.hpp"
 #include <filesystem>
 #include <iostream>
 
 using namespace std;
 
-class MinimalPublisher : public rclcpp::Node
-{
-
-public:
-    MinimalPublisher()
-
-        : Node("minimal_publisher")
-
-    {
-
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-
-        timer_ = this->create_wall_timer(
-
-            std::chrono::milliseconds(500),
-
-            std::bind(&MinimalPublisher::timer_callback, this));
-    }
-
-private:
-    void timer_callback()
-
-    {
-
-        auto message = std_msgs::msg::String();
-
-        message.data = "Hello, world! " + std::to_string(count_++);
-
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-
-        publisher_->publish(message);
-    }
-
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-
-    rclcpp::TimerBase::SharedPtr timer_;
-
-    size_t count_ = 0;
-
-};
+//extern Robot_Hardware *E_Hand;
+//int hardware_init(string ADDR);
 
 int main(int argc, char *argv[])
-
 {
+    UDP_Def UDP_Def_one;
+    Init_One_UDP_Connect(&UDP_Def_one, inet_addr("127.0.0.1"), htons(16201));
 
-    // UDP_Def UDP_Def_one;
-    // Init_One_UDP_Connect(&UDP_Def_one, inet_addr("127.0.0.1"), htons(16201));
 
-    // Robot_Hardware *Test_Robot = new Robot_Hardware();
-    // Test_Robot->Add_Device_Type("Switch_Board", Switch_Board_Device_Init, Switch_Board_Device_CallBack_F, Switch_Board_Device_Delete_F);
-    // Test_Robot->Add_Device_Type("TaiHu_Custom_Motor", Motor_Device_Init_TaiHu, Motor_Device_CallBack_TaiHu, Motor_Device_Delete_TaiHu);
+    rclcpp::init(argc, argv);
 
-    // filesystem::path exe_path = filesystem::canonical("/proc/self/exe");  
-    // filesystem::path dir_path = exe_path.parent_path();
-    // std::cout << "程序所在目录: " << dir_path << std::endl;
+    //hardware_init("src/devices_pkg/src/sdk/config/YAML/E_Bot/li_wenzhi/Servo_Config/TOP.yaml");
 
-    // string ADDR = dir_path.string() + "/../config/YAML/W_Bot/zhang_RuiMing/Motor/TaiHu_TOP.yaml";
-    // if(Test_Robot->Init_TOP(ADDR) != 0)
-    // {
-    //     cout << "Init_ERR" << endl;
-    //     return -1;
-    // }
+    //const auto ebot_node = std::make_shared<E_Bot_Node>(E_Hand);
 
-    // const shared_ptr<Device_Struct> Main_Switch_Board = Test_Robot->Get_Device_For_Name("Main_Switch_Board");
-    // Main_B *Main_Switch_Board_Control = static_cast<Main_B *>(Test_Robot->Get_Control_Class(Main_Switch_Board));
+    // 创建多线程执行器
+    //rclcpp::executors::MultiThreadedExecutor executor;
 
-    // const shared_ptr<Device_Struct> Motor = Test_Robot->Get_Device_For_Name("TiaHu_Motor");
-    // Motor_TaiHu *Motor_TaiHu_c = static_cast<Motor_TaiHu *>(Test_Robot->Get_Control_Class(Motor));
+    //executor.add_node(ebot_node);
+    //executor.spin();
 
-    // rclcpp::init(argc, argv);
-
-    // rclcpp::spin(std::make_shared<MinimalPublisher>());
-
-    // rclcpp::shutdown();
+    rclcpp::shutdown();
 
     return 0;
 }
--- src/devices_pkg/src/project/X_Hand_Node/X_Hand_Node.hpp
+++ src/devices_pkg/src/project/X_Hand_Node/X_Hand_Node.hpp
@@ -6,51 +6,41 @@
 #include "Switch_Board.hpp"
 #include <filesystem>
 #include <iostream>
-#include "e_bot_msg.hpp"
+#include "devices_pkg/msg/x_hand_msg.hpp"
+#include "Feetech_SCS0009.hpp"
 
 using namespace std;
 
-class E_Bot_Publisher : public rclcpp::Node
+
+extern shared_ptr<Device_Struct> Device_Servo_SCS0009;
+extern Servo_SCS0009 *Servo_Ctrl;
+
+class X_Hand_Node : public rclcpp::Node
 {
 public:
-    E_Bot_Publisher(Robot_Hardware *Robot_e_bot)
-    : Node("e_bot_publisher_node")
-    , Robot_ptr(Robot_e_bot)
-    {
-        publisher_ = this->create_publisher<devices_pkg::msg::EBotMsg>("e_bot_publisher", 10);
-        timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(10),
-            std::bind(&E_Bot_Publisher::timer_callback, this));
- }
-private:
-    void timer_callback()
-    {
-        auto message = devices_pkg::msg::EBotMsg();
-        message.frist_data = 1.23;
-        message.last_data = "E_Bot Message Data";
-         publisher_->publish(message);
-    }
-    rclcpp::Publisher<devices_pkg::msg::EBotMsg>::SharedPtr publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
-    Robot_Hardware *Robot_ptr;
-};
-
-
-class E_Bot_Subscriber : public rclcpp::Node
-{
-public:
-     E_Bot_Subscriber(Robot_Hardware *Robot_e_bot) 
-     : Node("e_bot_subscriber_node")
-     , Robot_ptr(Robot_e_bot)
+     X_Hand_Node(Robot_Hardware *Robot_x_hand)
+     : Node("x_hand_node")
+     , Robot_ptr(Robot_x_hand)
      {
-        subscription_ = this->create_subscription<devices_pkg::msg::EBotMsg>("e_bot_subscriber", 10, std::bind(&E_Bot_Subscriber::topic_callback, this, std::placeholders::_1));
+        subscription_ = this->create_subscription<devices_pkg::msg::XHandMsg>("x_hand_subscriber", 10, std::bind(&X_Hand_Node::topic_callback, this, std::placeholders::_1));
+        publisher_ = this->create_publisher<devices_pkg::msg::XHandMsg>("x_hand_publisher", 10);
      }
 
 private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    void topic_callback(const devices_pkg::msg::XHandMsg::SharedPtr msg) const
     {
-       // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+        //RCLCPP_INFO(this->get_logger(), "I heard[%d].", msg->cmd);
     }
-    rclcpp::Subscription<devices_pkg::msg::EBotMsg>::SharedPtr subscription_;
+
+    void publish_message()
+    {
+        auto message = devices_pkg::msg::XHandMsg();
+        // message.frist_data = 4.56;
+        // message.last_data = "X_Hand Node Message Data";
+        publisher_->publish(message);
+    }
+
+    rclcpp::Subscription<devices_pkg::msg::XHandMsg>::SharedPtr subscription_;
+    rclcpp::Publisher<devices_pkg::msg::XHandMsg>::SharedPtr publisher_;
     Robot_Hardware *Robot_ptr;
 };
